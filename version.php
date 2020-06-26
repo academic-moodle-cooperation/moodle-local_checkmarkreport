0d7b3da8553c3dfa7663fc3626b39567aebfdf36
Daniel <daniel.binder@univie.ac.at>
2020-06-26 22:14:30 +0200

    Fix dependency in version.php

--- version.php
+++ version.php
@@ -30,4 +30,4 @@ $plugin->requires  = 2020060700;
 $plugin->component = 'local_checkmarkreport';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v3.9.0';
-$plugin->dependencies = ['mod_checkmark' => 2019121100];
+$plugin->dependencies = ['mod_checkmark' => 2020060800];
