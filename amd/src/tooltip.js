4a1794c47c78cb693373942f1f8c3c840a3cb6a0
Angela Baier <angela.baier@univie.ac.at>
2023-08-28 13:53:33 +0200

    js fix

--- amd/src/tooltip.js
+++ amd/src/tooltip.js
@@ -16,7 +16,6 @@
 /**
  * Handles overlays/tooltips in report tables!
  *
- * @package   local_checkmarkreport
  * @author    Philipp Hager
  * @copyright 2014 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
