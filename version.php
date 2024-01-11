be9700ad9e0749e06594190cf40d40f2a65095c9
Angela Baier <angela.baier@univie.ac.at>
2024-01-11 10:20:00 +0100

    moodle required version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2024011100;
-$plugin->requires  = 2023042400;
+$plugin->requires  = 2023100900;
 $plugin->component = 'local_checkmarkreport';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v4.3.0';
